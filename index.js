aae3f197ff1294fa5a966bce55ab758134781908
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-03-03 23:23:35 +0000

    added engineer...with a bug

--- index.js
+++ index.js
@@ -12,4 +12,64 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const {Employee} = require('./lib/Employee');
 
+const team = [];
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter employee name:',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Enter employee ID:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter employee email:',
+      },
+      {
+        type: 'list',
+        name: 'role',
+        message: 'Select employee role:',
+        choices: ['Employee', 'Manager', 'Engineer', 'Intern'],
+      },
+    ])
+    .then((answers) => {
+      let employee;
+      switch (answers.role) {
+        case 'Employee':
+          employee = new Employee(answers.name, answers.id, answers.email);
+          break;
+        // Add cases for other roles here
+        default:
+          break;
+      }
+      team.push(employee);
+      console.log(`${answers.name} added to team!`);
+      inquirer
+        .prompt([
+          {
+            type: 'confirm',
+            name: 'addAnother',
+            message: 'Do you want to add another employee?',
+            default: true,
+          },
+        ])
+        .then((answers) => {
+          if (answers.addAnother) {
+            addEmployee();
+          } else {
+            console.log('Team:', team);
+          }
+        });
+    });
+}
+
+addEmployee();

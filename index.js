bf563e9ad0870973d7c009a1e82f0682009bf6da
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-03-05 20:16:05 +0000

    separated out functions according to employee title and created their own logs

--- index.js
+++ index.js
@@ -12,61 +12,121 @@ const render = require("./src/page-template.js");
 
 
 // Code to gather information about the development team members, and render the HTML file.
-const {Employee} = require('./lib/Employee');
+//const {Employee} = require('./lib/Employee');
 
 const team = [];
 
-function addEmployee() {
+const empQuestions = [
+  {
+    type: 'list',
+    name: 'role',
+    message: 'Select employee role:',
+    choices: ['Manager', 'Engineer', 'Intern'],
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Enter employee name:',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'Enter employee ID:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter employee email:',
+  },
+]
+
+const addEmpQuestion = [
+  {
+    type: 'confirm',
+    name: 'addAnother',
+    message: 'Do you want to add another employee?',
+    default: true,
+  },
+]
+const managerQuestion = 
+{
+  type: 'input',
+  name: 'officeNumber',
+  message: 'Please enter your office number:',
+}
+
+const engineerQuestion = 
+{
+  type: 'input',
+  name: 'github',
+  message: 'Please enter your github username:',
+}
+
+const internQuestion = 
+{
+  type: 'input',
+  name: 'school',
+  message: 'Please enter your school name:',
+}
+
+function addManager(otherEmpInfo){
+  inquirer.prompt(managerQuestion).then(answer => {
+    const employee = new Manager(otherEmpInfo.name, otherEmpInfo.id, otherEmpInfo.email, answer.officeNumber)
+    team.push(employee)
+    promptMenu()
+  })
+}
+
+function addEngineer(otherEmpInfo){
+  inquirer.prompt(engineerQuestion).then(answer => {
+    const employee = new Engineer(otherEmpInfo.name, otherEmpInfo.id, otherEmpInfo.email, answer.github)
+    team.push(employee)
+    promptMenu()
+  })
+}
+
+function addIntern(otherEmpInfo){
+  inquirer.prompt(internQuestion).then(answer => {
+    const employee = new Intern(otherEmpInfo.name, otherEmpInfo.id, otherEmpInfo.email, answer.school)
+    team.push(employee)
+    promptMenu()
+  })
+}
+function createHtml(){
+  //fs.writefile(FILENAME, DATA, ERR HANDLING)
+  fs.writeFile()
+}
+
+function promptMenu(){
   inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'role',
-        message: 'Select employee role:',
-        choices: ['Employee', 'Manager', 'Engineer', 'Intern'],
-      },
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Enter employee name:',
-      },
-      {
-        type: 'input',
-        name: 'id',
-        message: 'Enter employee ID:',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'Enter employee email:',
-      },
-    ])
-    .then((answers) => {
-      let employee;
-      switch (answers.role) {
-        case 'Employee':
-          employee = new Employee(answers.name, answers.id, answers.email);
-          break;
-      }
-      team.push(employee);
-      console.log(`${answers.name} added to team!`);
-      inquirer
-        .prompt([
-          {
-            type: 'confirm',
-            name: 'addAnother',
-            message: 'Do you want to add another employee?',
-            default: true,
-          },
-        ])
-        .then((answers) => {
-          if (answers.addAnother) {
+        .prompt(addEmpQuestion)
+        .then((answer) => {
+          
+          if (answer.addAnother) {
             addEmployee();
           } else {
-            console.log('Team:', team);
+            createHtml()
           }
         });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt(empQuestions)
+    .then((answers) => {
+      switch (answers.role) {
+        case 'Manager':
+          addManager(answers)
+        break;
+        case 'Intern':
+          addIntern(answers)
+        break;
+        case 'Engineer':
+          addEngineer(answers)
+        break;
+      }  
     });
 }
 
 addEmployee();
+
